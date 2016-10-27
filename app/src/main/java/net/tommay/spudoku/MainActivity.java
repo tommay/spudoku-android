80a823204c1748b54337237b9d2fa1e37e28e6db
Tom May <tom@tommay.net>
2016-10-27 15:27:47 -0700

    Hook up an onClick handler to the top left circle.

--- app/src/main/java/net/tommay/spudoku/MainActivity.java
+++ app/src/main/java/net/tommay/spudoku/MainActivity.java
@@ -2,12 +2,18 @@ package net.tommay.spudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Spudoku", "onCreate");
+    }
+
+    public void clicked(View view) {
+        Log.i("Spudoku", "clicked");
     }
 }
