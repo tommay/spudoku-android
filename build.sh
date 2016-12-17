5f09265ed49012cd515c9164b1c60ab4a70ca8f8
Tom May <tom@tommay.net>
2016-12-16 19:02:51 -0800

    Add build.sh to build, sign, and deply because Android Studio will be choking somewhere.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gradle assembleRelease &&
+jarsigner -sigalg SHA1withRSA -digestalg SHA1 \
+  -keystore ~/.android/debug.keystore -storepass android \
+  app/build/outputs/apk/app-release-unsigned.apk androiddebugkey &&
+~/Android/Sdk/platform-tools/adb install -r \
+  app/build/outputs/apk/app-release-unsigned.apk
