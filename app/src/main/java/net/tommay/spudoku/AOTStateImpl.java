bda7fd1410a9664497a793526c322e52950930cb
Tom May <tom@tommay.net>
2016-12-17 14:42:08 -0800

    Add comment about eventually using sqlite for AOTState.

--- app/src/main/java/net/tommay/spudoku/AOTStateImpl.java
+++ app/src/main/java/net/tommay/spudoku/AOTStateImpl.java
@@ -5,6 +5,25 @@ import android.content.SharedPreferences;
 import net.tommay.spudoku.RawPuzzle;
 import net.tommay.util.AOTState;
 
+/**
+ * Persists the state of an AOTProducer in a SharedPReferences.
+ * Eventually this should be done with sqlite.  When puzzles have
+ * difficulty ratings, we can create a puzzle with a given layout,
+ * rate it, put it with the rating, and repeat until we get the
+ * right rating.
+ * This scheme doesn't delete puzzles until a new one is inserted.
+ * Actually that may not make sense.  What's the used of allowing N
+ * puzzles of a given rating if we always return the first one?
+ * get:
+ *   fetch one row
+ *   if no row then return nul
+ *   update row set gotten = true
+ *   return a RawPuzzle
+ * set:
+ *   delete where rating = ? and gotten = true
+ *   select count(*) where rating = ?
+ *   if count < some limit then insert
+ */
 class AOTStateImpl implements AOTState<RawPuzzle> {
     private static final String KEY_PUZZLE = "puzzle";
     private static final String KEY_SOLUTION = "solution";
