fc697da791d864c640bf09d9a3d753e0f1ab3ffe
Tom May <tom@tommay.net>
2017-01-05 05:13:53 -0800

    Add jarsigner to install.sh.

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+jarsigner \
+  -sigalg SHA1withRSA \
+  -digestalg SHA1 \
+  -keystore ~/.android/debug.keystore \
+  -storepass android \
+  app/build/outputs/apk/app-release-unsigned.apk \
+  androiddebugkey
+
 ~/Android/Sdk/platform-tools/adb install -r \
   app/build/outputs/apk/app-dev-unsigned.apk
