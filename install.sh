59b1082e4bb2ea3f4a4014428e6cc8d64ba0a92a
Tom May <tom@tommay.net>
2017-01-16 02:12:28 -0800

    install.sh takes an argument for dev/release/etc.

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+type="$1"
+[ "$type" ] || type=release
+
 jarsigner \
   -sigalg SHA1withRSA \
   -digestalg SHA1 \
   -keystore ~/.android/debug.keystore \
   -storepass android \
-  app/build/outputs/apk/app-release-unsigned.apk \
+  app/build/outputs/apk/app-${type}-unsigned.apk \
   androiddebugkey
 
 ~/Android/Sdk/platform-tools/adb install -r \
-  app/build/outputs/apk/app-dev-unsigned.apk
+  app/build/outputs/apk/app-${type}-unsigned.apk
