b96291f9724f59ff1a5b78ae385620e1ff782b6b
Tom May <tom@tommay.net>
2017-01-18 17:38:39 -0800

    Add createWicked to CreaterForJava.scala.

--- scala/external/src/main/scala/net/tommay/sudoku/CreaterForJava.scala
+++ scala/external/src/main/scala/net/tommay/sudoku/CreaterForJava.scala
@@ -1,5 +1,7 @@
 package net.tommay.sudoku
 
+import scala.util.Random
+
 // Provide a "create" function intended to be called from Java that
 // does all the heavy lifting like Random creation and Layout lookup
 // in Scala.
@@ -29,5 +31,41 @@ object CreaterForJava {
           rnd, layout, Solver.solutions(options))
         (puzzle.toString, solution.puzzle.toString)
     }
+  }
+
+  def createWicked(randomSeed: Int, layoutName: String)
+      : (String, String) =
+  {
+    val rnd = new Random(randomSeed)
+    val options = new SolverOptions(
+      List(Heuristic.Forced, Heuristic.Needed, Heuristic.Tricky), false, true)
+    val solveFunc = Solver.solutions(options)(_)
+    Layout.getLayout(layoutName) match {
+      case None => ("", "")
+      case Some(layout) =>
+        val puzzles = createStreamWithSolution(rnd, layout, solveFunc)
+        val puzzlesRequiringGuessing = puzzles.filter{case (_, solution) =>
+          solution.steps.exists(_.tjpe == Heuristic.Guess)
+        }
+        val (puzzle, solution) = puzzlesRequiringGuessing.head
+        (puzzle.toString, solution.puzzle.toString)
+    }
+  }
+
+  def createStreamWithSolution(
+    rnd: Random,
+    layout: Iterable[Iterable[Int]],
+    solveFunc: Puzzle => Stream[Solution])
+      : Stream[(Puzzle, Solution)] =
+  {
+    val (rnd1, rnd2) = Util.split(rnd)
+    Creater.createWithSolution(rnd1, layout, solveFunc) #::
+      createStreamWithSolution(rnd2, layout, solveFunc)
+  }
+
+  def main(args: Array[String]) {
+    val seed = System.currentTimeMillis.toInt
+    val (puzzle, solved) = createWicked(seed, "classic")
+    println(s"$puzzle $solved")
   }
 }
