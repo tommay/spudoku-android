7b038c807b5783e461fff84fe2228d417df9c465
Tom May <tom@tommay.net>
2018-12-17 00:55:52 -0800

    Layout functions return Set[Int] instead of Iterable[Int] to be clear there are no duplicates.

--- scala/external/src/main/scala/net/tommay/sudoku/Layout.scala
+++ scala/external/src/main/scala/net/tommay/sudoku/Layout.scala
@@ -1,11 +1,11 @@
 package net.tommay.sudoku
 
 /* The functions in this list take a position from 0 to 81 and return
-   a List (actually an Iterable) of all positions that should have
-   their colors removed together when creating a puzzle.  */
+   a Set of all positions that should have their colors removed
+   together when creating a puzzle. */
 
 object Layout {
-  val layoutList : List[(String, Int => Iterable[Int])] =
+  val layoutList : List[(String, Int => Set[Int])] =
     List(
       "classic (half turn)" -> classic,
       "quarter turn" -> quarterTurn,
@@ -21,8 +21,8 @@ object Layout {
       "random" -> random
     )
 
-  def classic(n: Int) : Iterable[Int] = {
-    List(n, 80 - n)
+  def classic(n: Int) : Set[Int] = {
+    Set(n, 80 - n)
   }
 
   def reflectLeftRight(n: Int) : Int = {
@@ -30,8 +30,8 @@ object Layout {
     n - col + 8 - col
   }
 
-  def leftRight(n: Int) : Iterable[Int] = {
-    List(n, reflectLeftRight(n))
+  def leftRight(n: Int) : Set[Int] = {
+    Set(n, reflectLeftRight(n))
   }
 
   def reflectUpDown(n: Int) : Int = {
@@ -40,24 +40,24 @@ object Layout {
     n - rowish + 72 - rowish
   }
 
-  def leftRightUpDown(n: Int) : Iterable[Int] = {
+  def leftRightUpDown(n: Int) : Set[Int] = {
     val leftRightSets = leftRight(n)
     leftRightSets ++ leftRightSets.map(reflectUpDown)
   }
 
-  def identicalSquares(n: Int) : Iterable[Int] = {
+  def identicalSquares(n: Int) : Set[Int] = {
     val col = n % 3
     val row = (n / 9) % 3
     val base = row*9 + col
-    List(0, 3, 6, 27, 30, 33, 54, 57, 60).map(base + _)
+    Set(0, 3, 6, 27, 30, 33, 54, 57, 60).map(base + _)
   }
 
-  def quarterTurn(n: Int) : Iterable[Int] = {
-    spinny(List.empty, n)
+  def quarterTurn(n: Int) : Set[Int] = {
+    spinny(Set.empty, n)
   }
 
-  def spinny(result: List[Int], n: Int) : Iterable[Int] = {
-    // Rotate n 90 degrees, cons it, and recurse until we've come back
+  def spinny(result: Set[Int], n: Int) : Set[Int] = {
+    // Rotate n 90 degrees, add it, and recurse until we've come back
     // to where we started.
     if (result.contains(n)) {
       result
@@ -66,12 +66,12 @@ object Layout {
       val (row, col) = rowcol(n)
       val rowPrime = col
       val colPrime = 8 - row
-      spinny(n :: result, rowPrime * 9 + colPrime)
+      spinny(result + n, rowPrime * 9 + colPrime)
     }
   }
 
-  def random(n: Int) : Iterable[Int] = {
-    List(n)
+  def random(n: Int) : Set[Int] = {
+    Set(n)
   }
 
   def reflectDiagonally(n: Int) : Int = {
@@ -81,8 +81,8 @@ object Layout {
     rowPrime * 9 + colPrime
   }
 
-  def diagonal(n: Int) : Iterable[Int] = {
-    List(n, reflectDiagonally(n))
+  def diagonal(n: Int) : Set[Int] = {
+    Set(n, reflectDiagonally(n))
   }
 
   def reflectOtherDiagonally(n: Int) : Int = {
@@ -92,44 +92,44 @@ object Layout {
     rowPrime * 9 + colPrime
   }
 
-  def otherDiagonal(n: Int) : Iterable[Int] = {
-    List(n, reflectOtherDiagonally(n))
+  def otherDiagonal(n: Int) : Set[Int] = {
+    Set(n, reflectOtherDiagonally(n))
   }
 
-  def doubleDiagonal(n: Int) : Iterable[Int] = {
+  def doubleDiagonal(n: Int) : Set[Int] = {
     val diagonalSets = diagonal(n)
     diagonalSets ++ diagonalSets.map(reflectOtherDiagonally)
   }
 
-  def kaleidoscope(n: Int) : Iterable[Int] = {
+  def kaleidoscope(n: Int) : Set[Int] = {
     quarterTurn(n) ++ quarterTurn(reflectLeftRight(n))
   }
 
-  def tumbler(n: Int) : Iterable[Int] = {
+  def tumbler(n: Int) : Set[Int] = {
     val tumbled = List(
-      List(0, 5, 26, 51, 60, 59, 74, 45),
-      List(1, 14, 25, 42, 61, 68, 73, 36),
-      List(2, 23, 24, 33, 62, 77, 72, 27),
-      List(9, 4, 17, 52, 69, 58, 65, 46),
-      List(10, 13, 16, 43, 70, 67, 64, 37),
-      List(11, 22, 15, 34, 71, 76, 63, 28),
-      List(18, 3, 8, 53, 78, 57, 56, 47),
-      List(19, 12, 7, 44, 79, 66, 55, 38),
-      List(20, 21, 6, 35, 80, 75, 54, 29),
-      List(30, 32, 50, 48),
-      List(31, 41, 49, 39),
-      List(40)
+      Set(0, 5, 26, 51, 60, 59, 74, 45),
+      Set(1, 14, 25, 42, 61, 68, 73, 36),
+      Set(2, 23, 24, 33, 62, 77, 72, 27),
+      Set(9, 4, 17, 52, 69, 58, 65, 46),
+      Set(10, 13, 16, 43, 70, 67, 64, 37),
+      Set(11, 22, 15, 34, 71, 76, 63, 28),
+      Set(18, 3, 8, 53, 78, 57, 56, 47),
+      Set(19, 12, 7, 44, 79, 66, 55, 38),
+      Set(20, 21, 6, 35, 80, 75, 54, 29),
+      Set(30, 32, 50, 48),
+      Set(31, 41, 49, 39),
+      Set(40)
     )
-    tumbled.find(_ . contains(n)).getOrElse(List.empty)
+    tumbled.find(_.contains(n)).getOrElse(Set.empty)
   }
 
-  def barcode(n: Int) : Iterable[Int] = {
+  def barcode(n: Int) : Set[Int] = {
     val (row, col) = rowcol(n);
     val row0 = row - (row % 3)
-    List(0, 1, 2).map{n => (row0 + n) * 9 + col}
+    Set(0, 1, 2).map{n => (row0 + n) * 9 + col}
   }
 
-  def getLayout(name: String) : Option[Iterable[Iterable[Int]]] = {
+  def getLayout(name: String) : Option[Iterable[Set[Int]]] = {
     val layoutMap = layoutList
       .map{case (name, func) => (name.toLowerCase, func)}
       .toMap
@@ -137,7 +137,7 @@ object Layout {
       case None => None
       case Some(func) =>
         val sets = (0 to 80).map(func(_))
-        Some(uniqBy(sets, {x:Iterable[Int] => x.toList.min}))
+        Some(uniqBy(sets, {x:Set[Int] => x.min}))
     }
   }
 
@@ -147,10 +147,6 @@ object Layout {
 
   def rowcol(n: Int) : (Int, Int) = {
     (n / 9, n % 9)
-  }
-
-  def uniq[T](list: Iterable[T]): Iterable[T] = {
-    list.toSet
   }
 
   def uniqBy[T,K](list: Iterable[T], func: T => K) : Iterable[T] = {
