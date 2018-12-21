ad62a9e8c99807915d356e7b84874b2a83f2f739
Tom May <tom@tommay.net>
2018-12-21 10:15:09 -0800

    build..sh sets the java tools path and creates the debug keystore if necessary.

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+PATH=~/android-studio/jre/bin:"$PATH"
+
+KEYSTORE=~/.android/debug.keystore
+
+if [ ! -e $KEYSTORE ]; then
+  keytool -genkey -v -keystore $KEYSTORE \
+    -alias androiddebugkey -storepass android -keypass android \
+    -keyalg RSA -keysize 2048 -validity 10000 \
+    -dname "CN=Android Debug,O=Android,C=US"
+fi
+
 gradle assembleDev &&
 jarsigner \
   -sigalg SHA1withRSA \
   -digestalg SHA1 \
-  -keystore ~/.android/debug.keystore \
+  -keystore $KEYSTORE \
   -storepass android \
   app/build/outputs/apk/app-dev-unsigned.apk \
   androiddebugkey &&
