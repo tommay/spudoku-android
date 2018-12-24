5215732a4a78276afd6ceedb07fb00bbf4a1f5a9
Tom May <tom@tommay.net>
2018-12-23 16:38:15 -0800

    Add debug build type to build.gradle so I can build a version that works for "adb shell / run-as net.tommay.spudoku". Add an argument to build.sh so it can build dev or debug.

--- build.sh
+++ build.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
+# There are currently two targets, dev and debug.
+
+# Use debug so "adb shell / run-as net.tommay.spudoku works and allows
+# access to the apps files in /data/data/net.tommay.spudoku.  This
+# build also enables create.log and uses the same seed for the random
+# number generate to create the same puzzles so the create times are
+# consistent.
+
+# Use dev when you want random puzzles.
+
 PATH=~/android-studio/jre/bin:"$PATH"
 
-KEYSTORE=~/.android/debug.keystore
-
-if [ ! -e $KEYSTORE ]; then
-  keytool -genkey -v -keystore $KEYSTORE \
-    -alias androiddebugkey -storepass android -keypass android \
-    -keyalg RSA -keysize 2048 -validity 10000 \
-    -dname "CN=Android Debug,O=Android,C=US"
+if [ "$1" ]; then
+  target="$1"
+else
+  target=debug
 fi
 
-gradle assembleDev &&
-jarsigner \
-  -sigalg SHA1withRSA \
-  -digestalg SHA1 \
-  -keystore $KEYSTORE \
-  -storepass android \
-  app/build/outputs/apk/app-dev-unsigned.apk \
-  androiddebugkey &&
-adb install -r \
-  app/build/outputs/apk/app-dev-unsigned.apk
+case "$target" in
+  dev)
+    KEYSTORE=~/.android/debug.keystore
+
+    if [ ! -e $KEYSTORE ]; then
+      keytool -genkey -v -keystore $KEYSTORE \
+        -alias androiddebugkey -storepass android -keypass android \
+        -keyalg RSA -keysize 2048 -validity 10000 \
+        -dname "CN=Android Debug,O=Android,C=US"
+    fi
+
+    gradle assembleDev &&
+    jarsigner \
+      -sigalg SHA1withRSA \
+      -digestalg SHA1 \
+      -keystore $KEYSTORE \
+      -storepass android \
+      app/build/outputs/apk/app-dev-unsigned.apk \
+      androiddebugkey &&
+    adb install -r \
+      app/build/outputs/apk/app-dev-unsigned.apk
+    ;;
+  debug)
+    gradle assembleDebug &&
+    adb install -r \
+      app/build/outputs/apk/app-debug.apk
+    ;;
+  *)
+    echo "usage: $0 dev|debug"
+    ;;
+esac
