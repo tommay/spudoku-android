7111e87eb9b3f6394467bf68be22d94337f3d6d3
Tom May <tom@tommay.net>
2019-02-05 12:09:03 -0800

    Implement PersistentSpinner.

--- app/src/main/java/net/tommay/spudoku/PersistentSpinner.java
+++ app/src/main/java/net/tommay/spudoku/PersistentSpinner.java
@@ -1,25 +1,34 @@
 package net.tommay.spudoku;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
+/**
+ * A Spinner that uses SharedPreferences to persist the selection
+ * across runs.
+ */
 public class PersistentSpinner
     extends Spinner
 {
     private static final boolean LOG = net.tommay.spudoku.Log.LOG;
     private static final String TAG = net.tommay.spudoku.Log.TAG;
 
+    private final SharedPreferences _sharedPreferences;
     private final String _name;
 
-/*
-    public PersistentSpinner (Context context) {
-        super(context);
-        if (LOG) Log.i(TAG, "PersistentSpinner(Context)");
-    }
-*/
+    // We use our own OnItemSelectedListener to persist the selected
+    // item when it is selected, so keep a copy of the real listener
+    // here and forward events to it.
+
+    private AdapterView.OnItemSelectedListener _onItemSelectedListener = null;
 
     public PersistentSpinner (Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -37,10 +46,65 @@ public class PersistentSpinner
                 throw new RuntimeException(
                     "PersistentSpinner requires an xml name attribute");
             }
-            Log.i(TAG, "name: " + _name);
         }
         finally {
             a.recycle();
         }
+
+        _sharedPreferences = context.getSharedPreferences(
+            this.getClass().getName(), Context.MODE_PRIVATE);
+
+        super.setOnItemSelectedListener(
+            new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(
+                    AdapterView<?> parent, View view, int position, long id)
+                {
+                    if (_onItemSelectedListener != null) {
+                        _onItemSelectedListener.onItemSelected(
+                            parent, view, position, id);
+                    }
+                    String selected = (String) getSelectedItem();
+                    _sharedPreferences
+                        .edit()
+                        .putString(_name, selected)
+                        .apply();
+                    Log.i(TAG, "saved " + selected + " to preferences for " +
+                        _name);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    throw new RuntimeException(
+                        "Unexpected call to onNothingSelected");
+                }
+            });
+    }
+
+    @Override
+    public void setAdapter(SpinnerAdapter adapter) {
+        super.setAdapter(adapter);
+
+        // Now that we know the items to select from, select the item
+        // that was saved to preferences.
+
+        String selected = _sharedPreferences.getString(_name, null);
+        for (int i = 0, n = getCount(); i < n; i++) {
+            String item = (String) getItemAtPosition(i);
+            Log.i(TAG, "Item " + i + ": " + item);
+            if (item.equals(selected)) {
+                Log.i(TAG, "found selected at position " + i);
+                setSelection(i);
+            }
+        }
+    }
+
+    // Save the OnItemSelectedListener to forward to.
+    //
+    @Override
+    public void setOnItemSelectedListener(
+        AdapterView.OnItemSelectedListener listener)
+    {
+        _onItemSelectedListener = listener;
     }
 }
