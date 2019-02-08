56e19481085e9e4851506782e94feb950d56573b
Tom May <tom@tommay.net>
2019-02-08 15:23:10 -0800

    Using html to format the help text.

--- app/src/main/java/net/tommay/spudoku/HelpActivity.java
+++ app/src/main/java/net/tommay/spudoku/HelpActivity.java
@@ -1,8 +1,13 @@
 package net.tommay.spudoku;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 
 // Auto-generated from aoo/build.gradle settings.
 import net.tommay.spudoku.BuildConfig;
@@ -19,5 +24,27 @@ public class HelpActivity
         if (LOG) Log.i(TAG, "HelpActivity#onCreate");
 
         setContentView(R.layout.activity_help);
+
+        TextView tv = (TextView) findViewById(R.id.help_view);
+
+        tv.setMovementMethod(new ScrollingMovementMethod());
+
+        tv.setText(fromHtml(getString(R.string.help)));
+    }
+
+    public void onClickOk(View cellView) {
+        finish();
+    }
+
+    // XXX Available as androidx.core.text.HtmlCompat.fromHtml().
+    //
+    @SuppressWarnings("deprecation")
+    private static Spanned fromHtml(String source) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
+        }
+        else {
+            return Html.fromHtml(source);
+        }
     }
 }
