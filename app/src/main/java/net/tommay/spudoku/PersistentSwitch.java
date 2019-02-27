1f438bd654079174a36ca971dc71ec1ee9f42b0e
Tom May <tom@tommay.net>
2019-02-26 19:40:56 -0800

    PersistentSwitch now uses a "name" attribute instead of an AtomicInteger.

--- app/src/main/java/net/tommay/spudoku/PersistentSwitch.java
+++ app/src/main/java/net/tommay/spudoku/PersistentSwitch.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import net.tommay.util.Attributes;
 
 /**
  * A Switch that uses SharedPreferences to persist the state across
@@ -19,10 +19,8 @@ public class PersistentSwitch
     private static final boolean LOG = net.tommay.spudoku.Log.LOG;
     private static final String TAG = net.tommay.spudoku.Log.TAG;
 
-    private static final AtomicInteger _count = new AtomicInteger(0);
-
     private final SharedPreferences _sharedPreferences;
-    private final String _name = Integer.toString(_count.incrementAndGet());
+    private final String _name;
 
     // We use our own OnCheckedChangeListener to persist the checked
     // state when it has changed, so keep a copy of the real listener
@@ -34,8 +32,22 @@ public class PersistentSwitch
     public PersistentSwitch (Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        _name = Attributes.getAttribute(
+            context, attrs,
+            R.styleable.PersistentSwitch,
+            R.styleable.PersistentSwitch_name);
+
+        // Sadly there is no way to enforce required xml
+        // attributes so they can be caught at build time.
+        if (_name == null) {
+            throw new RuntimeException(
+                "PersistentSwitch requires an xml name attribute");
+        }
+
         _sharedPreferences = context.getSharedPreferences(
             this.getClass().getName(), Context.MODE_PRIVATE);
+
+
 
         if (_sharedPreferences.contains(_name)) {
             setChecked(_sharedPreferences.getBoolean(_name, false));
