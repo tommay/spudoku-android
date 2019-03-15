ad9cca7f696b020966221a7ab56428ede8ca60db
Tom May <tom@tommay.net>
2019-03-15 11:38:46 -0700

    Give up on the problematic Toolbar and just use a ConstraintLayout.

--- app/src/main/java/net/tommay/spudoku/MainActivity.java
+++ app/src/main/java/net/tommay/spudoku/MainActivity.java
@@ -23,7 +23,6 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
@@ -160,13 +159,6 @@ public class MainActivity
         if (LOG) Log.i(TAG, "onCreate");
 
         setContentView(R.layout.activity_main);
-
-        // Set the toolbar as the app bar for this Activity.
-
-        {
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-        }
 
         // Get Context-dependent colors resources.
 
